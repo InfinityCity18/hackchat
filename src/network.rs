d0868bc45477aa94baab3eac0e3dbe9cbf7c76eb
InfinityCity18 <kubawlas1@gmail.com>
2024-08-31 23:45:50 +0200

    added networking module

--- src/network.rs
+++ src/network.rs
@@ -1,7 +1,18 @@
-use std::net::UdpSocket;
+use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
 
-fn udp_manager(addr: &str) -> Result<(), std::io::Error> {
-    let socket = UdpSocket::bind(addr)?;
+const PORT: u16 = 7312;
+const SIGNATURE: &str = "github.com/InfinityCity18/hackchat";
+
+pub enum OpCode {
+    Message = 0,
+    User = 1,
+    Leave = 2,
+}
+
+pub fn udp_manager() -> Result<(), std::io::Error> {
+    let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, PORT))?;
     socket.set_broadcast(true)?;
+    socket.connect(SocketAddr::from((Ipv4Addr::BROADCAST, PORT)))?;
+
     Ok(())
 }
